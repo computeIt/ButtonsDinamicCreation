c6690b972e20f2a6bb61c184e2f757bbc9e73b60
Andrew Domingo <pushok@mail.ru>
2017-10-15 22:20:04 +0300

    with active buttons

--- app/src/main/java/com/example/addy/buttonsdinamiccreation/MainActivity.java
+++ app/src/main/java/com/example/addy/buttonsdinamiccreation/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.addy.buttonsdinamiccreation;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,6 +14,9 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText input;
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        int i=0;
+        Button button = new Button(this);
         switch (v.getId()){
             case R.id.createButton:
                 //creating layoutParams for setting up button properties
@@ -66,18 +74,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     case R.id.blue:
                         btnColor = Color.BLUE;
                         break;
+                    default:
+                        break;
                 }
 
-                Button button = new Button(this);
+                button = new Button(this);
                 button.setText(input.getText().toString());
                 button.setBackgroundColor(btnColor);
-                main_field.addView(button, layoutParams);//addind new button to the view(on the screen)
+                button.setId(i++);
+                button = (Button) findViewById(i-1);
+                main_field.addView(button, layoutParams);//adding new button to the view
+                button.setOnClickListener(this);
                 break;
             case R.id.clearButton:
                 main_field.removeAllViews();
-                Toast.makeText(MainActivity.this, "aff buttons deleted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "all buttons deleted", Toast.LENGTH_SHORT).show();
                 break;
             default:
+//                if(v.getId()>=0 && v.getId()<i){
+//                    ColorDrawable color = (ColorDrawable) button.getBackground();
+//                    int col = color.getColor();
+//                    main_field.setBackgroundColor(col);
+//                    Toast.makeText(this, "you pressed button with id #" + Integer.toString(v.getId()) + " with color=" + color.toString(), Toast.LENGTH_SHORT)
+//                        .show();
+//                }
                 break;
         }
     }
